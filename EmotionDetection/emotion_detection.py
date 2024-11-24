90d85ff93480a230a050cecde88e7105c2672b51
Fernando-AriasNunez <ariasnunez.fernando@gmail.com>
2024-11-24 18:43:53 -0500

    Add error handling and pylint check

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -12,22 +12,36 @@ def emotion_detector(text_to_analize):
 
     # Output formatting
     formatted_response = json.loads(response.text)
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
 
-    # Dominant emotion logic
-    emotion_response = {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score,
-    }
-    dominant_emotion = max(emotion_response, key = lambda x: emotion_response[x])
-    emotion_response['dominant_emotion'] = dominant_emotion
+    # Status handling
+    # Good response
+    if response.status_code == 200:
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
 
+        # Dominant emotion logic
+        emotion_response = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score
+        }
+        dominant_emotion = max(emotion_response, key = lambda x: emotion_response[x])
+        emotion_response['dominant_emotion'] = dominant_emotion
+
+    # Bad response
+    elif response.status_code == 400:
+        emotion_response = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     return emotion_response
     
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" This module serves the server API """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,18 +7,33 @@ app = Flask('Emotion Detector')
 
 @app.route('/')
 def render_index_page():
+    """ Function to render index page """
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def sent_detection():
+    """ Function to handle emotionDetector route """
     # Extract text to analize
     text_to_analize = request.args.get('textToAnalyze')
 
     # Pass the request to the emotion_detector function
     response = emotion_detector(text_to_analize)
 
-    # Extract returned values and build client response
-    client_response = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    # Client response construction
+    # Error handling
+    if response['dominant_emotion'] is None:
+        client_response = "Invalid text! Please try again!"
+    else:
+        # Extract returned values and build client response
+        client_response = (
+            "For the given statement, the system response is" +
+            " 'anger': " + str(response['anger']) +
+            ", 'disgust': " + str(response['disgust']) +
+            ", 'fear': " + str(response['fear']) +
+            ", 'joy': " + str(response['joy']) +
+            " and 'sadness': " + str(response['sadness']) +
+            ". The dominant emotion is " + str(response['dominant_emotion'])
+        )
 
     return client_response
 
